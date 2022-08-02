43e8609760cf1a87df1efd037c140f17810f3cbf
Alex Shabalin <shabalin.media@gmail.com>
2022-08-02 23:50:34 +0600

    Added WeatherManager and WeatherData.

---
+++ Clima/Moldel/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Alexander Shabalin on 2/8/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
---
+++ Clima/Moldel/WeatherManager.swift
@@ -0,0 +1,55 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Alexander Shabalin on 2/8/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=0e9d547aeb1971b3cad6e15816401559&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        // 1. Create a URLSession
+        if let url = URL(string: urlString) {
+            // 2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            // 3. Give the session a task
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                    
+                }
+            }
+            // 4. Start the task
+            task.resume()
+            
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+            
+        } catch {
+            print(error)
+        }
+        
+        
+    }
+    
+}
